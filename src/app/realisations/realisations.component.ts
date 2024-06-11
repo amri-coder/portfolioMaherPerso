4d9b9651a7811bd8a16ed62d996997f64e8a6e09
Maher <amri.site@yahoo.com>
2024-06-11 20:11:59 +0200

    Portfolio Maher

--- src/app/realisations/realisations.component.ts
+++ src/app/realisations/realisations.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-realisations',
   templateUrl: './realisations.component.html',
-  styleUrls: ['./realisations.component.css']
+  styleUrls: ['./realisations.component.css'],
 })
 export class RealisationsComponent {
-
+  amriDev: any = '../../assets/amri-dev.png';
 }
